71a8d345018503d3372a1419c707b63b84f6b736
Roan Richmond <roan.richmond@codethink.co.uk>
2023-12-15 12:25:33 +0000

    assembler.txt: Getting input from file
    Also writing the result to file.
    
    Signed-off-by: Roan Richmond <roan.richmond@codethink.co.uk>

--- assembler.cpp
+++ assembler.cpp
@@ -1,17 +1,31 @@
 #include <string>
 #include <iostream>
+#include <fstream>
 
 #include "instructions.hpp"
 
 using namespace std;
 
+int binary_to_file(string target, string dest) 
+{
+	ifstream read_file(target);
+	ofstream write_file(dest);
+	string curr_line;
+
+	while (getline(read_file, curr_line)) {
+		instruction ins = string_to_instr(curr_line);
+		display_instr(ins);
+		string bin_ins = instr_to_string(ins);
+		write_file << bin_ins << "\n";
+	}
+
+	read_file.close();
+	write_file.close();
+
+}
+
 int main (void) 
 {
-	string test1 = "  ADDI x2, x1,   2000 ";
-	instruction ins = string_to_instr(test1);
-	cout << "String to Ins Complete" << endl;
-	display_instr(ins); 
-	string ans = instr_to_string(ins);
-	cout << ans; 
+	binary_to_file("test.txt", "res.txt");
 	return 0;
 }
